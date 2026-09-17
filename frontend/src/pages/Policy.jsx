76b051ef7c9e69c01d45d366ad6750d542c6eea5
Gemy <omargamal1132004@gmail.com>
2026-09-17 21:34:08 +0300

    update policy'

--- frontend/src/pages/Policy.jsx
+++ frontend/src/pages/Policy.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 
 const Policy = () => {
   return (
@@ -8,23 +7,31 @@ const Policy = () => {
         Return & Exchange Policy
       </h1>
       <ul className="list-disc pl-6 space-y-4 text-lg">
+        <li>The original receipt should be available.</li>
         <li>
-          All returns, refunds and size exchanges must be done within a maximum
-          of five days of receiving your product.
+          You can Exchange/ Refund within 14 days from the date of purchase.
+        </li>
+        <li>
+          Delivery fees are applied on any cancellation or return orders if the order is already shipped.
+        </li>
+        <li>
+          The product has to be in its Original and unused status.
+        </li>
+        <li>
+          The refunded amount will take place by the same payment method that you used in the purchase transaction ( cash or visa ).
+        </li>
+        <li>
+          We can not Exchange or return these products (inner caps– turban – basic wear – pins – extensions – hijab accessories- Burkini-Cover Ups).
+        </li>
+        <li>
+          For items purchased under promotions or discounts, returns are not accepted; however, exchanges are allowed within 3 days of purchase.
         </li>
         <li>
           We would like to inform you that as per our courier’s policy, opening
-          the package upon delivery is not permitted. Unfortunately, this means
-          you won’t be able to inspect or separate items upon receipt. Rest
-          assured, we understand the importance of customer satisfaction. If,
-          for any reason, you wish to return or exchange an item, you have a
-          generous window of 5 days to reach out to us. Our dedicated customer
-          service team will be more than happy to assist you with any concerns
-          or requests.
+          the package upon delivery is not permitted. Rest assured, if you wish to return or exchange an item, you have a window of 14 days to reach out to us.
         </li>
         <li>
-          Reach out to us via DM or email in case you decided to
-          return/exchange.
+          Reach out to us via DM or email in case you decided to return/exchange.
         </li>
         <li>
           All returns and exchanges for orders in Cairo will take place at our
@@ -33,26 +40,16 @@ const Policy = () => {
           Cairo.
         </li>
         <li>
-          Returned/exchanged items must be returned new, unused, unwashed and
-          with all garment tags attached otherwise we won’t be able to neither
-          return nor exchange the product.
-        </li>
-        <li>
           For return and exchange orders outside Cairo, the customer pays for
           the shipping fees of the item returning back to us in case of a
-          return. In case of an exchange the clients pays for both return and
-          forward items
+          return. In case of an exchange the client pays for both return and
+          forward items.
         </li>
         <li>
-          For international orders, it’s important to note that once your order
-          arrives in your country, it may be subjected to customs regulations
-          and fees, which are determined by your country’s rules and
-          regulations. These additional charges are beyond our control and are
-          the responsibility of the recipient.
+          For international orders, once your order arrives in your country, it may be subjected to customs regulations and fees, which are determined by your country’s rules and regulations. These additional charges are beyond our control and are the responsibility of the recipient.
         </li>
         <li>
-          Exchange and returns are only allowed as long as the products are
-          still in stock.
+          Exchange and returns are only allowed as long as the products are still in stock.
         </li>
       </ul>
     </div>
@@ -60,3 +57,4 @@ const Policy = () => {
 };
 
 export default Policy;
+
